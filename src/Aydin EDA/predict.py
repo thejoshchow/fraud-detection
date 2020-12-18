867b86b5ac184753dbbb51a8ce0500813bb90008
aydin-hasanli <aydin.hasanli.peng@gmail.com>
2020-12-18 12:08:22 -0600

    added 2 function to predict.py

--- src/Aydin EDA/predict.py
+++ src/Aydin EDA/predict.py
@@ -1,12 +1,28 @@
 import pandas as pd
 import numpy as np
 import pickle
-from model import get_model, get_transform_data
+from model import get_transform_data
 
+def get_model(filepath='rf.pkl'):
+    "Unpickles and returns model"
+    return pickle.load(open('rf.pkl','rb'))
+
+def mypred(filepath, modelpath):
+    X, y = get_transform_data(filepath)
+    X = X.fillna(0)
+    model = get_model(modelpath)
+    y_rf = model.predict(X)
+    y_rf_proba = model.predict_proba(X)
+    return y_rf, y_rf_proba
 
 if __name__ == '__main__':
-    X, y = get_transform_data('test_script_examples.json')
-    X = X.fillna(0)
-    model = get_model('rf.pkl')
-    y_rf = model.predict(X)
+    # X, y = get_transform_data('test_script_examples.json')
+    # X = X.fillna(0)
+    # model = get_model('rf.pkl')
+    # y_rf = model.predict(X)
+    # y_rf_proba = model.predict_proba(X)
+    # print(y_rf)
+    # print(y_rf_proba)
+    y_rf, y_rf_proba=mypred('test_script_examples.json', 'rf.pkl')
     print(y_rf)
+    print(y_rf_proba)
